1a10d5abcb6b47c41fc07e0b667da8a1cb4c2241
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 08:51:34 -0800

    async Await whit Task<T>

---
+++ MyClass.cs
@@ -0,0 +1,18 @@
+﻿namespace _005_AsyncAwait
+{
+    internal class MyClass
+    {
+        int Operation()
+        {
+            Console.WriteLine("Operation in Thread Id = {0}", Environment.CurrentManagedThreadId);
+
+            return 2 + 2;
+        }
+
+        public async void OperationAsync()
+        {
+            Task<int> task = Task<int>.Factory.StartNew(Operation);
+            Console.WriteLine("\n Result {0}", await task);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using _005_AsyncAwait;
+
+MyClass myClass = new();
+
+myClass.OperationAsync();
+
+Console.WriteLine("General Thread finish the work ThreadId = {0}", Environment.CurrentManagedThreadId);
+Console.ReadLine();
